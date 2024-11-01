6cd36484df5e48b330f79ef1f391305f13aa7490
Bashir Ahmed <160276094+Bashirahmed377@users.noreply.github.com>
2024-11-01 19:36:33 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Footer(){
+    return(
+
+            <footer className="bg-black text-white text-center text-xl absolute bottom w-[100vw] p-4
+             font-bold">
+                Made by <Link href="https://www.linkedin.com/in/bashir-ahmed-1a49b223b/  " target="_blank">Bashir Ahmed</Link> 
+            </footer>
+
+    )
+}
